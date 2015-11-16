f949aa6a0a1e53afd71adddab20d75cf50f5ab27
Erich Snow <ewsnow@usfca.edu>
2015-11-15 20:49:07 -0800

    sure hope this is just the file i want

---
+++ Documents/Program_Experiments/mit_course/problem_set_0.py
@@ -0,0 +1,8 @@
+#problem set 0
+#write a problem that does the following in order:
+#asks the user to enter their date of birth
+#asks the user to enter their last name
+#prints out the user's last name and date of birth, in that order
+dob = raw_input('enter date of birth: ')
+name = raw_input ('enter last name: ')
+print name, dob
